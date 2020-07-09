2d4a98fe83d6ff3a62aa15e22700768c5a392756
navneet singh <nnavi.singh1@gmail.com>
2020-07-09 10:09:53 +0530

    update according month/dat

--- src/components/covid.vue
+++ src/components/covid.vue
@@ -1,14 +1,21 @@
 <template>
-  <div>
+  <div class="maincontainer">
     <h1>Hello! {{ name }}</h1>
-    <div>
-      <span>
-        <h3>Slide the slider for select the date</h3>
-      </span>
-      <vueSlider v-bind="sliderOption" v-model="value" @change="updatedMap"></vueSlider>
-      <h4>Your Selected date is: {{ value}} of march</h4>
+    <div class="heading">
+      <h3>Slide the slider for select the date</h3>
     </div>
-    <div>
+    <div class="monthslider">
+      <span>Month</span>
+      <vueSlider v-bind="sliderOption2" v-model="monthValue"  @change="updatedMapMonth"></vueSlider>
+    </div>
+    <div class="dateslider">
+      <span>Date</span>
+      <vueSlider v-bind="sliderOption1" v-model="value" @change="updatedMapDate"></vueSlider>
+    </div>
+    <div class="result">
+      <h4>Your Selected date is: {{ value}}-{{ monthValue }}</h4>
+    </div>
+    <div class="map">
       <l-map ref="mymap" @ready="initmap" :zoom="zoom" :center="center" style="height:500px">
         <l-tile-layer :url="url"></l-tile-layer>
         <l-geo-json :geojson="geojsondata" :options="options"></l-geo-json>
@@ -37,9 +44,11 @@ export default {
       center: [18.5204, 73.8567],
       geojsondata: data,
       dates: ["08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18"],
-      value: "08",
+      months: ['04','05','06'],
+      value: null,
+      monthValue: null,
       fillColor: "#e4ce7f",
-      sliderOption: {
+      sliderOption1: {
         dotSize: 15,
         width: "900px",
         height: 5,
@@ -47,7 +56,20 @@ export default {
         data: this.dates,
         min: 8,
         max: 18,
-        interval: 1
+        interval: 1,
+        marks: true
+      },
+      sliderOption2: {
+        dotSize: 15,
+        width: "900px",
+        height: 5,
+        direction: "ltr",
+        data: this.months,
+        min: 4,
+        max: 6,
+        interval: 1,
+        marks: true,
+        
       }
     };
   },
@@ -73,34 +95,57 @@ export default {
         ? "#31a354"
         : "#2ca25f";
     },
-    updatedMap(value) {
-      
-      if(value == 8 || value == 9){
-        this.value = "0"+value;
-      }
-      else{
+    updatedMapDate(value) {
+      //this.dateValue = dateValue;
+      if (value == 8 || value == 9) {
+        this.value = "0" + value;
+      } else {
         this.value = value;
       }
-      console.log('value', value)
+      console.log("value", this.value);
       let option = {
         style: this.styleFunction,
         onEachFeature: this.onEachFeatureFunction
       };
+      L.geoJSON(this.geojsondata, option).addTo(this.map);
+    },
+    updatedMapMonth(monthValue) {
+      if (
+        monthValue == 1 ||
+        monthValue == 2 ||
+        monthValue == 3 ||
+        monthValue == 4 ||
+        monthValue == 5 ||
+        monthValue == 6 ||
+        monthValue == 7 ||
+        monthValue == 8 ||
+        monthValue == 9
+      ) {
+        this.monthValue = "0" + monthValue;
+      } else {
+        this.monthValue = monthValue;
+      }
+      let option = {
+        style: this.styleFunction
+      };
+      console.log("monthValue", this.monthValue);
       L.geoJSON(this.geojsondata, option).addTo(this.map);
     }
   },
   computed: {
     options() {
       return {
-        style: this.styleFunction,
-        onEachFeature: this.onEachFeatureFunction
+        style: this.styleFunction
+        //onEachFeature: onEachFeatureFunction
       };
     },
     styleFunction() {
       let vm = this;
       return feature => {
         return {
-          fillColor: this.getColor(feature.properties["covid_04_" + vm.value]),
+          fillColor: this.getColor(
+            feature.properties["covid_" + vm.monthValue + "_" + vm.value]
+          ),
           weight: 2,
           opacity: 1,
           color: "white",
@@ -110,17 +155,25 @@ export default {
       };
     },
     onEachFeatureFunction() {
-      return (feature, layer) => {
-        layer.bindTooltip(
-          "<div>Ward Name:" +
-            feature.properties.ward_name +
-            "</div><div>no of case: " +
-            feature.properties.covid_04_08 +
-            "</div>",
-          { permanent: false, sticky: true }
-        );
-      };
+      return (feature, layer) => {};
     }
   }
 };
 </script>
+<style scoped>
+.monthslider {
+  top: 20px;
+  height: 50px;
+  position: relative;
+}
+.dateslider {
+  top: 30px;
+  height: 50px;
+  position: relative;
+}
+.result {
+  top: 30px;
+  height: 50px;
+  position: relative;
+}
+</style>
